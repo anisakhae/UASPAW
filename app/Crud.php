cd589c3d179bc4b8105539e410f72aa19f8e2ccd
gita02 <cahyagita2@gmail.com>
2018-12-22 14:08:02 +0700

    ica

---
+++ app/Crud.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Crud extends Model
+{
+    protected $table = 'crud';
+    protected $primarykey = 'id';
+    protected $fillable = ['judul', 'isi'];
+    public $timestamps = false;
+
+}
---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Crud;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+class CrudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datas = Crud::orderBy('id', 'DESC')->paginate(3);
+        return view('show')->with('datas',$datas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $tambah = new Crud();
+        $tambah->judul = $request->get('judul');
+        //Judul kita jadikan slug
+        $tambah->slug_judul = Str::slug($request->get('judul'));
+        $tambah->isi = $request->get('isi');
+        // Disini proses mendapatkan judul dan memindahkan letak gambar ke folder image
+        $file       = $request->file('gambar');
+        $fileName   = $file->getClientOriginalName();
+        $request->file('gambar')->move("image/", $fileName);
+
+        $tambah->gambar = $fileName;
+        $tambah->save();
+
+        return redirect()->to('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $tampilkan = Crud::where('slug_judul', $slug)->first();
+        return view('tampil')->with('tampilkan', $tampilkan);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tampiledit = Crud::where('id', $id)->first();
+        return view('edit')->with('tampiledit', $tampiledit);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = Crud::where('id', $id)->first();
+        $update->judul = $request['judul'];
+        $update->isi = $request['isi'];
+
+        if($request->file('gambar') == "")
+        {
+            $update->gambar = $update->gambar;
+        } 
+        else
+        {
+            $file       = $request->file('gambar');
+            $fileName   = $file->getClientOriginalName();
+            $request->file('gambar')->move("image/", $fileName);
+            $update->gambar = $fileName;
+        }
+        
+        $update->update();
+        return redirect()->to('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $hapus = Crud::find($id);
+        $hapus->delete();
+
+        return redirect()->to('/');
+    }
+}
